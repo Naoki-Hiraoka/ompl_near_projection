4094e7487a5c230f6c7b91199023ffe94aa616a9
Naoki-Hiraoka <hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2023-04-18 22:49:18 +0900

    fix comment

--- src/NearGoalSpace.cpp
+++ src/NearGoalSpace.cpp
@@ -2,7 +2,7 @@
 
 namespace ompl_near_projection{
   bool NearGoalSpace::sampleTo(ompl::base::State *state, const ompl::base::State *source) const{
-    NearProjectedStateSpacePtr goalSpaceNear = std::static_pointer_cast<NearProjectedStateSpace>(goalSpace_); // goal spaceは、NearProjectedStateSpaceであるという想定
+    NearProjectedStateSpacePtr goalSpaceNear = std::static_pointer_cast<NearProjectedStateSpace>(goalSpace_); // goal spaceは、NearProjectedStateSpaceであるという想定. 本当はGoalSpace::setSpace時にcastして保管しておきたいのだが、GoalSpace::setSpaceがvirtual関数として宣言されていないのでできなかった.
 
     bool ret = goalSpaceNear->getNearConstraint()->projectNearValid(state, source);
     si_->enforceBounds(state);
