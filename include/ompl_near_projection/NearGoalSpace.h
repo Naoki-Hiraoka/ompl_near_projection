762bfe1a48bc649a0b3ca6caf4743a6a9bb185c6
Naoki Hiraoka <32383525+Naoki-Hiraoka@users.noreply.github.com>
2023-04-18 13:16:55 +0900

    Update NearGoalSpace.h

--- include/ompl_near_projection/NearGoalSpace.h
+++ include/ompl_near_projection/NearGoalSpace.h
@@ -10,7 +10,7 @@ namespace ompl_near_projection{
     NearGoalSpace(const ompl::base::SpaceInformationPtr &si) : GoalSpace(si) {}
 
     // sourceをGoalSpaceのconstraintへprojectionしたものをstateとして返す.
-    // projection失敗してもそのままprojection後の値をstateとして返す. 返り値はfalseになる. stateがStateSpaceを満たすためには、sourceがambientSpaceを満たしていて、GoalSpaceのprojectionが「ambientSpaceの制約を満たす範囲内で可能な限りGoalSpaceへprojetionする」という仕様である必要がある.
+    // projection失敗してもそのままprojection後の値をstateとして返す. 返り値はfalseになる. stateがambientSpaceを満たすためには、sourceがambientSpaceを満たしていて、GoalSpaceのprojectionが「ambientSpaceの制約を満たす範囲内で可能な限りGoalSpaceへprojetionする」という仕様である必要がある.
     // 返り値のstateは、sourceからstateへの、ambientSpaceを満たすmotionが存在するという保証がある
     bool sampleTo(ompl::base::State *state, const ompl::base::State *source) const;
   };
